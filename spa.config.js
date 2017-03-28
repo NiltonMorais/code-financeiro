fde10ae6ba1d69f84e9f97d1321e37a1a847a843
NiltonMorais <nilton_even@hotmail.com>
2017-03-28 02:20:01 -0300

    modificando para testar no heroku

--- spa.config.js
+++ spa.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  'api_url': process.env.API_URL,
+  //'api_url': process.env.API_URL,
+  'api_url': 'http://codefinanceiro.heroku.com/api',
   'app_path': '/app',
   'login_path': '#!/login'
 };
