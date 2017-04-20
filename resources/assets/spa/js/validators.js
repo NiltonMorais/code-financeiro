8cff0c2413f29f959cb4fd2335f308f1de62ab43
NiltonMorais <nilton_even@hotmail.com>
2017-04-20 18:29:45 -0300

    traduzindo messagens de validacao para pt-br

--- resources/assets/spa/js/validators.js
+++ resources/assets/spa/js/validators.js
@@ -1,3 +1,11 @@
 import VeeValidate from 'vee-validate';
+import dicPtMessages from './locale/validator/pt-br';
 
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+    locale: 'pt-br',
+    dictionary: {
+        'pt-br': {
+            messages: dicPtMessages
+        }
+    }
+});
