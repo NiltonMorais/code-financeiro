906ccb827a30339ebfb273fae27523687ea57699
NiltonMorais <nilton_even@hotmail.com>
2017-05-19 00:13:50 -0300

    comentando chamadas das seeds de plano de contas

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,13 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(ClientsTableSeeder::class);
-        sleep(3);
         $this->call(UsersTableSeeder::class);
         $this->call(BankAccountsTableSeeder::class);
         $this->call(CategoryExpensesTableSeeder::class);
         $this->call(CategoryRevenuesTableSeeder::class);
-        $this->call(BillPaysTableSeeder::class);
-        $this->call(BillReceivesTableSeeder::class);
+        //$this->call(BillPaysTableSeeder::class);
+        //$this->call(BillReceivesTableSeeder::class);
         $this->call(PlansTableSeeder::class);
     }
 }
