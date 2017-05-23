934022dd1450e01fd6f1aa035a081c89e47e9f91
NiltonMorais <nilton_even@hotmail.com>
2017-05-23 16:13:50 -0300

    adicionando bind da class OrderRepository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -34,6 +34,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(\CodeFin\Repositories\Interfaces\StatementRepository::class, \CodeFin\Repositories\StatementRepositoryEloquent::class);
         $this->app->bind(\CodeFin\Repositories\Interfaces\PlanRepository::class, \CodeFin\Repositories\PlanRepositoryEloquent::class);
         $this->app->bind(\CodeFin\Repositories\Interfaces\SubscriptionRepository::class, \CodeFin\Repositories\SubscriptionRepositoryEloquent::class);
+        $this->app->bind(\CodeFin\Repositories\Interfaces\OrderRepository::class, \CodeFin\Repositories\OrderRepositoryEloquent::class);
         //:end-bindings:
     }
 }
