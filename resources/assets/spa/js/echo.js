b10e8042fe86dd97718dd3505366a2419cce6c33
NiltonMorais <nilton_even@hotmail.com>
2017-05-22 23:32:55 -0300

    retirando chamada do pusher-js no echo.js

--- resources/assets/spa/js/echo.js
+++ resources/assets/spa/js/echo.js
@@ -1,20 +1,18 @@
-import Echo from 'laravel-echo';
-import JwtToken from './services/jwt-token';
-
-window.Pusher = require('pusher-js');
+import Echo from "laravel-echo";
+import JwtToken from "./services/jwt-token";
 
 window.Echo = new Echo({
-   broadcaster: 'pusher',
-   key: '722fe87d08a56a1d5599'
+    broadcaster: 'pusher',
+    key: '722fe87d08a56a1d5599'
 });
 
 const changeJwtTokenInEcho = value => {
-   window.Echo.connector.pusher.config.auth.headers['Authorization'] =
-       JwtToken.getAuthorizationHeader();
+    window.Echo.connector.pusher.config.auth.headers['Authorization'] =
+        JwtToken.getAuthorizationHeader();
 };
 
 JwtToken.event('updateToken', value => {
-   changeJwtTokenInEcho(value);
+    changeJwtTokenInEcho(value);
 });
 
 changeJwtTokenInEcho(JwtToken.token);
